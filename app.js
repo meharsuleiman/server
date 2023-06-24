baf8c0db1ce2ec3cf157bfc098cfc147aa7ae94f
mehar sulaiman <dr.love9026@gmail.com>
2023-06-24 20:29:28 +0500

    finalize

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import hpp from 'hpp';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // ENVIRONMENT variables
 import { config } from 'dotenv';
@@ -13,11 +17,20 @@ export const app = express();
 // * Body Parser, reading data from request's body
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
 app.use(
   cors({
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
-    origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
+    // origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
+    origin: '*',
+  })
+);
+app.use(
+  hpp({
+    whitelist: ['category', 'keyword'],
   })
 );
 
